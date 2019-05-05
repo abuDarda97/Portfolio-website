7d8995f24c3c9d7223868a8ec07002ae9b6960bf
Abu Darda <abu.d@hotmail.co.uk>
2019-05-05 17:34:05 +0100

    intial setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h1>Helllo</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Hellllllllllllo />,
+  document.getElementById('app')
+);
