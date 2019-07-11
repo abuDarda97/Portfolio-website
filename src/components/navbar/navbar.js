8e60ef0f4d1838a193fc5123fd00a18ba8e3c513
Abu Darda <abu.d@hotmail.co.uk>
2019-07-11 22:07:12 +0100

    navnar burger menu added

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,14 +6,26 @@ const Navbar = () => {
     <div>
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo right">/A</a>
-          <ul id="nav-mobile" className="left hide-on-med-and-down">
-            <li><a>About me</a></li>
-            <li><a>Projects</a></li>
-            <li><a>Contact</a></li>
-          </ul>
+          <div className="container">
+            <a href="#" className="brand-logo right">/A</a>
+            <a className="sidenav-trigger" data-target="mobile">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li><a>About me</a></li>
+              <li><a>Projects</a></li>
+              <li><a>Contact</a></li>
+            </ul>
+          </div>
         </div>
       </nav>
+
+      <ul id="mobile" className="sidenav">
+        <li><a>About me</a></li>
+        <li><a>Projects</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+
     </div>
   );
 };
