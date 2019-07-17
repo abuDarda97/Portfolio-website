5a6a2592f422060a64c79e221c9b308075c2755e
Abu Darda <abu.d@hotmail.co.uk>
2019-07-17 22:08:21 +0100

    toggle button component created

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './navbar.css';
 
-const Navbar = () => {
+import ToggleButton from '../toggleButton/toggleButton';
+
+const Navbar = (props) => {
   return (
     <div>
       <nav>
         <div className="nav-wrapper">
           <div className="container">
+            <ToggleButton />
             <a href="#" className="brand-logo right">/A</a>
-            <a className="sidenav-trigger" data-target="mobile">
-              <i className="material-icons">menu</i>
-            </a>
-            <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <ul id="nav" className="left hide-on-med-and-down">
               <li><a>About me</a></li>
               <li><a>Projects</a></li>
               <li><a>Contact</a></li>
@@ -19,13 +19,6 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-
-      <ul id="mobile" className="sidenav">
-        <li><a>About me</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Contact</a></li>
-      </ul>
-
     </div>
   );
 };
---
+++ src/components/toggleButton/toggleButton.js
@@ -0,0 +1,12 @@
+import './toggleButton.css';
+import React from 'react';
+
+const ToggleButton = () => {
+  return (
+    <a className="sidenav-trigger" data-target="mobile">
+      <i className="material-icons">menu</i>
+    </a>
+  );
+};
+
+export default ToggleButton;
