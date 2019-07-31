16149765424a3708dffb9ce38e74512c562a527a
Abu Darda <abu.d@hotmail.co.uk>
2019-07-31 21:27:26 +0100

    navbar design updated

--- src/components/Navbar/Mobile/Mobile.js
+++ src/components/Navbar/Mobile/Mobile.js
@@ -3,18 +3,42 @@ import React from 'react';
 import './Mobile.css';
 
 const MobileNav = props => {
-  if(props.status) {
-    return (
-      <ul className='mobile__navbar'>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <ul className={props.status ? 'mobile__navbar--expanded':'mobile__navbar--collapsed'}>
+      <li>
+        <a className='mobile__navbar__list' href='/'>Home</a>
+      </li>
+      <li>
+        <a className='mobile__navbar__list' href='/about'>About</a>
+      </li>
+      <li>
+        <a className='mobile__navbar__list' href='/projects'>Projects</a>
+      </li>
+      <li>
+        <a className='mobile__navbar__list' href='/contact'>Contact</a>
+      </li>
+    </ul>
+  );
+  // if(props.status) {
+  //   return (
+  //     <ul className='mobile__navbar'>
+  //       <li>
+  //         <a className='mobile__navbar__list' href='/'>Home</a>
+  //       </li>
+  //       <li>
+  //         <a className='mobile__navbar__list' href='/about'>About</a>
+  //       </li>
+  //       <li>
+  //         <a className='mobile__navbar__list' href='/projects'>Projects</a>
+  //       </li>
+  //       <li>
+  //         <a className='mobile__navbar__list' href='/contact'>Contact</a>
+  //       </li>
+  //     </ul>
+  //   );
+  // } else {
+  //   return null;
+  // }
 };
 
 export default MobileNav;
