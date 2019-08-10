1311f0fd2838d7fec5ba1a1b22ba152e0d220b5b
Abu Darda <abu.d@hotmail.co.uk>
2019-08-10 18:59:07 +0100

    projects page fully responsive

--- src/components/Navbar/Mobile/Mobile.js
+++ src/components/Navbar/Mobile/Mobile.js
@@ -19,26 +19,6 @@ const MobileNav = props => {
       </li>
     </ul>
   );
-  // if(props.status) {
-  //   return (
-  //     <ul className='mobile__navbar'>
-  //       <li>
-  //         <a className='mobile__navbar__list' href='/'>Home</a>
-  //       </li>
-  //       <li>
-  //         <a className='mobile__navbar__list' href='/about'>About</a>
-  //       </li>
-  //       <li>
-  //         <a className='mobile__navbar__list' href='/projects'>Projects</a>
-  //       </li>
-  //       <li>
-  //         <a className='mobile__navbar__list' href='/contact'>Contact</a>
-  //       </li>
-  //     </ul>
-  //   );
-  // } else {
-  //   return null;
-  // }
 };
 
 export default MobileNav;
--- src/pages/projects/cards/cards.js
+++ src/pages/projects/cards/cards.js
@@ -6,22 +6,78 @@ import small from '../../../images/small.jpg';
 
 const Cards = props => {
   return (
-    <div className='card'>
-      <img className='imgStyle' src={ small } />
-      <h3>
+    <div className='grid'>
+      <div className='grid__items'>
+        <img className='imgStyle' src={ small } />
+        <h3>
         React Calc
-      </h3>
-      <p>
+        </h3>
+        <p>
         jeeeeeeeeejfvejfn jejlnejlnle
         keklfenlel j efjjfe jejellef
         fnjnfjnfejnfj ejn a jajja lane
-      </p>
-      <button>
-        <a href='/'>Source code</a>
-      </button>
-      <button>
-        <a href='/'>Demo</a>
-      </button>
+        </p>
+        <button>
+          <a href='/'>Source code</a>
+        </button>
+        <button>
+          <a href='/'>Demo</a>
+        </button>
+      </div>
+
+      <div className='grid__items'>
+        <img className='imgStyle' src={ small } />
+        <h3>
+        React Calc
+        </h3>
+        <p>
+        jeeeeeeeeejfvejfn jejlnejlnle
+        keklfenlel j efjjfe jejellef
+        fnjnfjnfejnfj ejn a jajja lane
+        </p>
+        <button>
+          <a href='/'>Source code</a>
+        </button>
+        <button>
+          <a href='/'>Demo</a>
+        </button>
+      </div>
+
+      <div className='grid__items'>
+        <img className='imgStyle' src={ small } />
+        <h3>
+        React Calc
+        </h3>
+        <p>
+        jeeeeeeeeejfvejfn jejlnejlnle
+        keklfenlel j efjjfe jejellef
+        fnjnfjnfejnfj ejn a jajja lane
+        </p>
+        <button>
+          <a href='/'>Source code</a>
+        </button>
+        <button>
+          <a href='/'>Demo</a>
+        </button>
+      </div>
+
+      <div className='grid__items'>
+        <img className='imgStyle' src={ small } />
+        <h3>
+        React Calc
+        </h3>
+        <p>
+        jeeeeeeeeejfvejfn jejlnejlnle
+        keklfenlel j efjjfe jejellef
+        fnjnfjnfejnfj ejn a jajja lane
+        </p>
+        <button>
+          <a href='/'>Source code</a>
+        </button>
+        <button>
+          <a href='/'>Demo</a>
+        </button>
+      </div>
     </div>
   );
 };
