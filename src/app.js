9a4ad0c999fadc120f4a4bedf8bd2f88ee375ee4
Abu Darda <abu.d@hotmail.co.uk>
2019-11-24 18:50:16 +0000

    using react bootstrap and now refactored navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Navbar from './components/Navbar/Navbar';
+import Navigation from './components/Navbar/Navigation';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navigation />
     </div>
   );
 };
