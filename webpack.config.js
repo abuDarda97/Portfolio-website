d8539d2d7a476c7f339f0b59ccff05279f387428
Abu Darda <abu.d@hotmail.co.uk>
2020-02-02 21:21:44 +0000

    server and webpack production mode created

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,12 @@ module.exports = {
   mode: 'development',
   devServer: {
     historyApiFallback: true,
-  }, 
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: 'src/index.html'
