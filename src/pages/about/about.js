12b1189f6fd0bff9e5301170114ac668b3113b84
Abu Darda <abu.d@hotmail.co.uk>
2020-03-07 19:28:23 +0000

    about me section completed

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navigation from '../../components/Navbar/Navigation';
-import { Card } from 'react-bootstrap';
 import Footer from '../../components/Footer/Footer';
 
 import './about.css';
@@ -10,16 +9,13 @@ const about = () => {
     <div>
       <Navigation />
       <div className='about__container'>
-        <h1>ABOUT ME:</h1>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Etiam bibendum turpis et fermentum sagittis. Nullam eu ex 
-          eu massa suscipit vulputate. Mauris cursus quam nec erat bibendum 
-          faucibus. Curabitur volutpat eu nulla vel porta. Curabitur feugiat 
-          neque in urna dignissim ultrices. Donec nec elit tempor, mattis urna 
-          eget, pulvinar eros. Nunc egestas tristique nulla sed iaculis. 
-          Nam ornare mi ac efficitur mattis. Aenean pharetra massa augue, 
-          quis hendrerit urna hendrerit nec. Mauris sit amet suscipit lectus. 
-          Maecenas euismod purus enim, convallis ullamcorper nibh sollicitudin eget.
+        <h3 id='title'>ABOUT ME:</h3>
+        <div className='about__text'>
+          I am a <span className='keyword'>passionate </span>fullstack software developer, 
+          I enjoy engineering <span className='keyword'>complex</span> applications. I work on 
+          both <span className='keyword'>backend</span> and <span className='keyword'>frontend</span>. When I am not querying databases 
+          or aligning pixels, I love watching Boxing and MMA. 
+        </div>
       </div>
       <Footer />
     </div>
