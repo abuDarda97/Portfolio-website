10d2d5e600492e4345f4174698effaaf3529689b
Abu Darda <abu.d@hotmail.co.uk>
2020-04-05 00:28:00 +0100

    image optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,24 @@ module.exports = {
                mozjpeg: {
                  progressive: true,
                  quality: 65
+               },
+               // optipng.enabled: false will disable optipng
+               optipng: {
+                 enabled: false,
+               },
+               pngquant: {
+                 quality: [0.65, 0.90],
+                 speed: 4
+               },
+               gifsicle: {
+                 interlaced: false,
+               },
+               // the webp option will enable WEBP
+               webp: {
+                 quality: 65,
+                 size:1000
                }
-             },
+             }
            }
          ]
       }
